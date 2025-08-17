510e31072be29d13ed9e26ab2229715c8c9a80fa
Samuel Petersson <samuel.petersson@hotmail.com>
2025-08-17 21:59:27 +0200

    Färdig autentisering med hjälp av JWT

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 mongoose.set("strictQuery", false)
@@ -48,7 +49,14 @@ router.post("/login", async (req,res) => {
         if(!isPasswordMatch){
             return res.status(401).json({error : "incorrect username/password"})
         }else{
-            res.status(200).json({message: "user logged in"})
+            const payload = {username:username}
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn:'1h'})
+            const response = {
+                message: "user logged in",
+                token: token
+
+            }
+            res.status(200).json({response})
         }
 
     } catch(error){
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const authRoutes = require("./routes/authRoutes")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const app = express()
@@ -9,6 +10,23 @@ app.use(bodyParser.json())
 
 
 app.use("/api", authRoutes)
+
+app.get("/api/protected", authenticateToken, (req, res) =>{
+    res.json({message: "Skyddad route"})
+})
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(token == null) res.status(401).json({message: "not authorized for this route - token missing"})
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({message: "Not correct JWT"})
+        
+        req.username = username
+        next()
+    })
+}
 
 app.listen(port, () => {
     console.log(`server running at ${port}`) 
